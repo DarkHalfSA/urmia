d41b997ac0d219e34dcaebd02979a20afa89d96f
DarkHalfSA <59757809+DarkHalfSA@users.noreply.github.com>
2020-02-07 00:33:06 +0330

    Create examples.py

---
+++ examples.py
@@ -0,0 +1,18 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+>>> from spacy.lang.aze.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "بو اؤرنک اولاراق بیر جمله‌دی.",
+    "قراریمیز، بوگون ساعات 2:30 فضولی خیاوانیندا.",
+    "دونن ،آیخان منه 2000 تومن پول وئردی.",
+    "تبریزدن ساویا نئجه گئتمک اولار؟",
+    "هوانین یوزده سکسنی نیتروژندن تشکیل تاپیر",
+]
